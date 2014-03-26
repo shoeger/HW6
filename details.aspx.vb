ea47e10b6b82ede132196d9fbd36c8d5caec064b
Sam Hoeger <sam-hoeger@uiowa.edu>
2014-03-26 17:38:43 -0500

    Added code behind for deleting

--- details.aspx.vb
+++ details.aspx.vb
@@ -7,7 +7,18 @@ Partial Class details
     End Sub
 
     Protected Sub DetailsView1_ItemDeleted(sender As Object, e As DetailsViewDeletedEventArgs) Handles DetailsView1.ItemDeleted
-        Response.Write("The record has been deleted from the Recipe Book")
+
+        Dim delRecipe As String = e.Values("reName").ToString()
+
+        Response.Write("The record")
+        Response.Write("<span class=deletedrecipeshighlight>")
+        Response.Write(delRecipe)
+        Response.Write("</span> has been delted from the Recipe Book")
+
+        Response.AddHeader("REFRESH", "3;URL=./Recipes.aspx")
+
+
+        ' Response.Write("The record has been deleted from the Recipe Book")
     End Sub
 
 
